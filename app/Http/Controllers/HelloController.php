98f77041cdbb96267865a897559109ddcfe111e7
marjunstudio <uebujie@gmail.com>
2023-06-11 17:46:30 +0900

    Add:RequestとResponseを取得

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 
 class HelloController extends Controller
 {
 	
-	public function __invoke() {
+	public function index(Request $request, Response $response) {
 
-		return <<<EOF
+			$html = <<<EOF
 <html>
 <head>
 <title>Hello/Index</title>
@@ -20,10 +21,15 @@ h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; 
 </style>
 </head>
 <body>
-	<h1>SingleAction</h1>
-	<p>これは、シングルコントローラのindexアクションです。</p>
+	<h1>Hello</h1>
+	<h3>Request</h3>
+	<pre>{$request}</pre>
+	<h3>Response</h3>
+	<pre>{$response}</pre>
 </body>
 </html>
 EOF;
+			$response->setContent($html);
+			return $response;
 	}
 }
